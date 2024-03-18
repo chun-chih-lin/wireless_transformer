a932441a7d9bd86410b7cbce40026b90619a1f8e
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-03-18 15:14:56 -0400

    update

--- gr-wireless_dump/python/wireless_dump/wifi_dump.py
+++ gr-wireless_dump/python/wireless_dump/wifi_dump.py
@@ -28,7 +28,7 @@ class wifi_dump(gr.sync_block):
 
         if self.count == 0:
             for tag in tags:
-                print(f"{tag = }")
+                print(f"{tag = } {type(tag) = }")
                 # Convert from PMT to python string
                 key = pmt.to_python(tag.key)
 
