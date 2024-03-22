430cceca7e6b65c8e35055032868e162c71b5637
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-03-22 15:49:29 -0400

    update

--- gr-wireless_dump/examples/WIFI_RX.py
+++ gr-wireless_dump/examples/WIFI_RX.py
@@ -5,42 +5,36 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Wiif Tx
+# Title: Wifi Rx
 # GNU Radio version: v3.10.9.2-5-gdd01ef52
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-import os
-import sys
-sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
-
 from PyQt5 import QtCore
 from PyQt5.QtCore import QObject, pyqtSlot
 from gnuradio import blocks
-import pmt
 from gnuradio import gr
 from gnuradio.filter import firdes
 from gnuradio.fft import window
+import sys
 import signal
 from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-from gnuradio import network
 from gnuradio import uhd
 import time
-from wifi_phy_hier import wifi_phy_hier  # grc-generated hier_block
-import foo
+from gnuradio import wireless_dump
 import ieee802_11
 
 
 
-class WIIF_TX(gr.top_block, Qt.QWidget):
+class WIFI_RX(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Wiif Tx", catch_exceptions=True)
+        gr.top_block.__init__(self, "Wifi Rx", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Wiif Tx")
+        self.setWindowTitle("Wifi Rx")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -58,7 +52,7 @@ class WIIF_TX(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "WIIF_TX")
+        self.settings = Qt.QSettings("GNU Radio", "WIFI_RX")
 
         try:
             geometry = self.settings.value("geometry")
@@ -70,23 +64,21 @@ class WIIF_TX(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.tx_gain = tx_gain = 0.75
-        self.sdr_addr = sdr_addr = "addr=192.168.20.2"
+        self.window_size = window_size = 48
+        self.sync_length = sync_length = 320
+        self.sdr_addr = sdr_addr = "addr=192.168.40.2"
         self.samp_rate = samp_rate = 20e6
-        self.pdu_length = pdu_length = 500
-        self.out_buf_size = out_buf_size = 96000
+        self.pdu_length = pdu_length = 10
         self.lo_offset = lo_offset = 0
-        self.interval = interval = 300
+        self.gain = gain = 0.75
         self.freq = freq = 2412000000
-        self.encoding = encoding = 0
+        self.chan_est = chan_est = 0
+        self.Debug = Debug = 0
 
         ##################################################
         # Blocks
         ##################################################
 
-        self._tx_gain_range = qtgui.Range(0, 1, 0.01, 0.75, 200)
-        self._tx_gain_win = qtgui.RangeWidget(self._tx_gain_range, self.set_tx_gain, "'tx_gain'", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._tx_gain_win)
         # Create the options list
         self._samp_rate_options = [5000000.0, 10000000.0, 20000000.0]
         # Create the labels list
@@ -103,9 +95,13 @@ class WIIF_TX(gr.top_block, Qt.QWidget):
             lambda i: self.set_samp_rate(self._samp_rate_options[i]))
         # Create the radio buttons
         self.top_layout.addWidget(self._samp_rate_tool_bar)
-        self._pdu_length_range = qtgui.Range(0, 1500, 1, 500, 200)
+        self._pdu_length_range = qtgui.Range(0, 1500, 1, 10, 200)
         self._pdu_length_win = qtgui.RangeWidget(self._pdu_length_range, self.set_pdu_length, "'pdu_length'", "counter_slider", int, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._pdu_length_win)
+        self.top_grid_layout.addWidget(self._pdu_length_win, 3, 0, 1, 1)
+        for r in range(3, 4):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
         # Create the options list
         self._lo_offset_options = [0, 6000000.0, 11000000.0]
         # Create the labels list
@@ -122,9 +118,9 @@ class WIIF_TX(gr.top_block, Qt.QWidget):
             lambda i: self.set_lo_offset(self._lo_offset_options[i]))
         # Create the radio buttons
         self.top_layout.addWidget(self._lo_offset_tool_bar)
-        self._interval_range = qtgui.Range(10, 1000, 1, 300, 200)
-        self._interval_win = qtgui.RangeWidget(self._interval_range, self.set_interval, "'interval'", "counter_slider", int, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._interval_win)
+        self._gain_range = qtgui.Range(0, 1, 0.01, 0.75, 200)
+        self._gain_win = qtgui.RangeWidget(self._gain_range, self.set_gain, "'gain'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._gain_win)
         # Create the options list
         self._freq_options = [2412000000.0, 2417000000.0, 2422000000.0, 2427000000.0, 2432000000.0, 2437000000.0, 2442000000.0, 2447000000.0, 2452000000.0, 2457000000.0, 2462000000.0, 2467000000.0, 2472000000.0, 2484000000.0, 5170000000.0, 5180000000.0, 5190000000.0, 5200000000.0, 5210000000.0, 5220000000.0, 5230000000.0, 5240000000.0, 5250000000.0, 5260000000.0, 5270000000.0, 5280000000.0, 5290000000.0, 5300000000.0, 5310000000.0, 5320000000.0, 5500000000.0, 5510000000.0, 5520000000.0, 5530000000.0, 5540000000.0, 5550000000.0, 5560000000.0, 5570000000.0, 5580000000.0, 5590000000.0, 5600000000.0, 5610000000.0, 5620000000.0, 5630000000.0, 5640000000.0, 5660000000.0, 5670000000.0, 5680000000.0, 5690000000.0, 5700000000.0, 5710000000.0, 5720000000.0, 5745000000.0, 5755000000.0, 5765000000.0, 5775000000.0, 5785000000.0, 5795000000.0, 5805000000.0, 5825000000.0, 5860000000.0, 5870000000.0, 5880000000.0, 5890000000.0, 5900000000.0, 5910000000.0, 5920000000.0]
         # Create the labels list
@@ -142,87 +138,117 @@ class WIIF_TX(gr.top_block, Qt.QWidget):
         # Create the radio buttons
         self.top_layout.addWidget(self._freq_tool_bar)
         # Create the options list
-        self._encoding_options = [0, 1, 2, 3, 4, 5, 6, 7]
+        self._Debug_options = [0, 1]
         # Create the labels list
-        self._encoding_labels = ['BPSK 1/2', 'BPSK 3/4', 'QPSK 1/2', 'QPSK 3/4', '16QAM 1/2', '16QAM 3/4', '64QAM 2/3', '64QAM 3/4']
+        self._Debug_labels = ['Disable', 'Enable']
         # Create the combo box
+        self._Debug_tool_bar = Qt.QToolBar(self)
+        self._Debug_tool_bar.addWidget(Qt.QLabel("'Debug'" + ": "))
+        self._Debug_combo_box = Qt.QComboBox()
+        self._Debug_tool_bar.addWidget(self._Debug_combo_box)
+        for _label in self._Debug_labels: self._Debug_combo_box.addItem(_label)
+        self._Debug_callback = lambda i: Qt.QMetaObject.invokeMethod(self._Debug_combo_box, "setCurrentIndex", Qt.Q_ARG("int", self._Debug_options.index(i)))
+        self._Debug_callback(self.Debug)
+        self._Debug_combo_box.currentIndexChanged.connect(
+            lambda i: self.set_Debug(self._Debug_options[i]))
         # Create the radio buttons
-        self._encoding_group_box = Qt.QGroupBox("'encoding'" + ": ")
-        self._encoding_box = Qt.QHBoxLayout()
-        class variable_chooser_button_group(Qt.QButtonGroup):
-            def __init__(self, parent=None):
-                Qt.QButtonGroup.__init__(self, parent)
-            @pyqtSlot(int)
-            def updateButtonChecked(self, button_id):
-                self.button(button_id).setChecked(True)
-        self._encoding_button_group = variable_chooser_button_group()
-        self._encoding_group_box.setLayout(self._encoding_box)
-        for i, _label in enumerate(self._encoding_labels):
-            radio_button = Qt.QRadioButton(_label)
-            self._encoding_box.addWidget(radio_button)
-            self._encoding_button_group.addButton(radio_button, i)
-        self._encoding_callback = lambda i: Qt.QMetaObject.invokeMethod(self._encoding_button_group, "updateButtonChecked", Qt.Q_ARG("int", self._encoding_options.index(i)))
-        self._encoding_callback(self.encoding)
-        self._encoding_button_group.buttonClicked[int].connect(
-            lambda i: self.set_encoding(self._encoding_options[i]))
-        self.top_layout.addWidget(self._encoding_group_box)
-        self.wifi_phy_hier_0 = wifi_phy_hier(
-            bandwidth=samp_rate,
-            chan_est=ieee802_11.LS,
-            encoding=encoding,
-            frequency=freq,
-            sensitivity=0.56,
-        )
-        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+        self.top_layout.addWidget(self._Debug_tool_bar)
+        self.wireless_dump_wifi_dump_0 = wireless_dump.wifi_dump(0, pdu_length, Debug)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
             ",".join(('', sdr_addr)),
             uhd.stream_args(
                 cpu_format="fc32",
                 args='',
                 channels=list(range(0,1)),
             ),
-            'packet_len',
         )
-        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_sink_0.set_time_now(uhd.time_spec(time.time()), uhd.ALL_MBOARDS)
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_time_unknown_pps(uhd.time_spec(0))
 
-        self.uhd_usrp_sink_0.set_center_freq(uhd.tune_request(freq, rf_freq = freq - lo_offset, rf_freq_policy=uhd.tune_request.POLICY_MANUAL), 0)
-        self.uhd_usrp_sink_0.set_normalized_gain(tx_gain, 0)
-        self.network_socket_pdu_0 = network.socket_pdu('TCP_SERVER', '', '52001', 10000, False)
-        self.ieee802_11_mac_0 = ieee802_11.mac([0x23, 0x23, 0x23, 0x23, 0x23, 0x23], [0x42, 0x42, 0x42, 0x42, 0x42, 0x42], [0xff, 0xff, 0xff, 0xff, 0xff, 255])
-        self.foo_packet_pad2_0 = foo.packet_pad2(False, False, 0.01, 100, 1000)
-        self.foo_packet_pad2_0.set_min_output_buffer(out_buf_size)
-        self.blocks_vector_source_x_0 = blocks.vector_source_c((0,), False, 1, [])
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_cc(0.6)
-        self.blocks_multiply_const_vxx_0.set_min_output_buffer(100000)
-        self.blocks_message_strobe_0_0 = blocks.message_strobe(pmt.intern("".join("x" for i in range(pdu_length))), interval)
+        self.uhd_usrp_source_0.set_center_freq(uhd.tune_request(freq, rf_freq = freq - lo_offset, rf_freq_policy=uhd.tune_request.POLICY_MANUAL), 0)
+        self.uhd_usrp_source_0.set_normalized_gain(gain, 0)
+        self.ieee802_11_sync_short_0 = ieee802_11.sync_short(0.56, 2, False, False)
+        self.ieee802_11_sync_long_0 = ieee802_11.sync_long(sync_length, False, False)
+        # Create the options list
+        self._chan_est_options = [0, 1, 2, 3]
+        # Create the labels list
+        self._chan_est_labels = ['LS', 'LMS', 'Linear Comb', 'STA']
+        # Create the combo box
+        # Create the radio buttons
+        self._chan_est_group_box = Qt.QGroupBox("'chan_est'" + ": ")
+        self._chan_est_box = Qt.QHBoxLayout()
+        class variable_chooser_button_group(Qt.QButtonGroup):
+            def __init__(self, parent=None):
+                Qt.QButtonGroup.__init__(self, parent)
+            @pyqtSlot(int)
+            def updateButtonChecked(self, button_id):
+                self.button(button_id).setChecked(True)
+        self._chan_est_button_group = variable_chooser_button_group()
+        self._chan_est_group_box.setLayout(self._chan_est_box)
+        for i, _label in enumerate(self._chan_est_labels):
+            radio_button = Qt.QRadioButton(_label)
+            self._chan_est_box.addWidget(radio_button)
+            self._chan_est_button_group.addButton(radio_button, i)
+        self._chan_est_callback = lambda i: Qt.QMetaObject.invokeMethod(self._chan_est_button_group, "updateButtonChecked", Qt.Q_ARG("int", self._chan_est_options.index(i)))
+        self._chan_est_callback(self.chan_est)
+        self._chan_est_button_group.buttonClicked[int].connect(
+            lambda i: self.set_chan_est(self._chan_est_options[i]))
+        self.top_layout.addWidget(self._chan_est_group_box)
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.blocks_moving_average_xx_1 = blocks.moving_average_cc(window_size, 1, 4000, 1)
+        self.blocks_moving_average_xx_0 = blocks.moving_average_ff((window_size  + 16), 1, 4000, 1)
+        self.blocks_divide_xx_0 = blocks.divide_ff(1)
+        self.blocks_delay_0_0 = blocks.delay(gr.sizeof_gr_complex*1, 16)
+        self.blocks_delay_0 = blocks.delay(gr.sizeof_gr_complex*1, sync_length)
+        self.blocks_conjugate_cc_0 = blocks.conjugate_cc()
+        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(1)
+        self.blocks_complex_to_mag_0 = blocks.complex_to_mag(1)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.msg_connect((self.blocks_message_strobe_0_0, 'strobe'), (self.ieee802_11_mac_0, 'app in'))
-        self.msg_connect((self.ieee802_11_mac_0, 'phy out'), (self.wifi_phy_hier_0, 'mac_in'))
-        self.msg_connect((self.network_socket_pdu_0, 'pdus'), (self.ieee802_11_mac_0, 'app in'))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.foo_packet_pad2_0, 0))
-        self.connect((self.blocks_vector_source_x_0, 0), (self.wifi_phy_hier_0, 0))
-        self.connect((self.foo_packet_pad2_0, 0), (self.uhd_usrp_sink_0, 0))
-        self.connect((self.wifi_phy_hier_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_complex_to_mag_0, 0), (self.blocks_divide_xx_0, 0))
+        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.blocks_moving_average_xx_0, 0))
+        self.connect((self.blocks_conjugate_cc_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.blocks_delay_0, 0), (self.ieee802_11_sync_long_0, 1))
+        self.connect((self.blocks_delay_0_0, 0), (self.blocks_conjugate_cc_0, 0))
+        self.connect((self.blocks_delay_0_0, 0), (self.ieee802_11_sync_short_0, 0))
+        self.connect((self.blocks_divide_xx_0, 0), (self.ieee802_11_sync_short_0, 2))
+        self.connect((self.blocks_moving_average_xx_0, 0), (self.blocks_divide_xx_0, 1))
+        self.connect((self.blocks_moving_average_xx_1, 0), (self.blocks_complex_to_mag_0, 0))
+        self.connect((self.blocks_moving_average_xx_1, 0), (self.ieee802_11_sync_short_0, 1))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_moving_average_xx_1, 0))
+        self.connect((self.ieee802_11_sync_long_0, 0), (self.wireless_dump_wifi_dump_0, 0))
+        self.connect((self.ieee802_11_sync_short_0, 0), (self.blocks_delay_0, 0))
+        self.connect((self.ieee802_11_sync_short_0, 0), (self.ieee802_11_sync_long_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_delay_0_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_multiply_xx_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "WIIF_TX")
+        self.settings = Qt.QSettings("GNU Radio", "WIFI_RX")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
 
         event.accept()
 
-    def get_tx_gain(self):
-        return self.tx_gain
+    def get_window_size(self):
+        return self.window_size
 
-    def set_tx_gain(self, tx_gain):
-        self.tx_gain = tx_gain
-        self.uhd_usrp_sink_0.set_normalized_gain(self.tx_gain, 0)
+    def set_window_size(self, window_size):
+        self.window_size = window_size
+        self.blocks_moving_average_xx_0.set_length_and_scale((self.window_size  + 16), 1)
+        self.blocks_moving_average_xx_1.set_length_and_scale(self.window_size, 1)
+
+    def get_sync_length(self):
+        return self.sync_length
+
+    def set_sync_length(self, sync_length):
+        self.sync_length = sync_length
+        self.blocks_delay_0.set_dly(int(self.sync_length))
 
     def get_sdr_addr(self):
         return self.sdr_addr
@@ -236,21 +262,14 @@ class WIIF_TX(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self._samp_rate_callback(self.samp_rate)
-        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
-        self.wifi_phy_hier_0.set_bandwidth(self.samp_rate)
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
 
     def get_pdu_length(self):
         return self.pdu_length
 
     def set_pdu_length(self, pdu_length):
         self.pdu_length = pdu_length
-        self.blocks_message_strobe_0_0.set_msg(pmt.intern("".join("x" for i in range(self.pdu_length))))
-
-    def get_out_buf_size(self):
-        return self.out_buf_size
-
-    def set_out_buf_size(self, out_buf_size):
-        self.out_buf_size = out_buf_size
+        self.wireless_dump_wifi_dump_0.set_pdu_len(self.pdu_length)
 
     def get_lo_offset(self):
         return self.lo_offset
@@ -258,14 +277,14 @@ class WIIF_TX(gr.top_block, Qt.QWidget):
     def set_lo_offset(self, lo_offset):
         self.lo_offset = lo_offset
         self._lo_offset_callback(self.lo_offset)
-        self.uhd_usrp_sink_0.set_center_freq(uhd.tune_request(self.freq, rf_freq = self.freq - self.lo_offset, rf_freq_policy=uhd.tune_request.POLICY_MANUAL), 0)
+        self.uhd_usrp_source_0.set_center_freq(uhd.tune_request(self.freq, rf_freq = self.freq - self.lo_offset, rf_freq_policy=uhd.tune_request.POLICY_MANUAL), 0)
 
-    def get_interval(self):
-        return self.interval
+    def get_gain(self):
+        return self.gain
 
-    def set_interval(self, interval):
-        self.interval = interval
-        self.blocks_message_strobe_0_0.set_period(self.interval)
+    def set_gain(self, gain):
+        self.gain = gain
+        self.uhd_usrp_source_0.set_normalized_gain(self.gain, 0)
 
     def get_freq(self):
         return self.freq
@@ -273,21 +292,27 @@ class WIIF_TX(gr.top_block, Qt.QWidget):
     def set_freq(self, freq):
         self.freq = freq
         self._freq_callback(self.freq)
-        self.uhd_usrp_sink_0.set_center_freq(uhd.tune_request(self.freq, rf_freq = self.freq - self.lo_offset, rf_freq_policy=uhd.tune_request.POLICY_MANUAL), 0)
-        self.wifi_phy_hier_0.set_frequency(self.freq)
+        self.uhd_usrp_source_0.set_center_freq(uhd.tune_request(self.freq, rf_freq = self.freq - self.lo_offset, rf_freq_policy=uhd.tune_request.POLICY_MANUAL), 0)
 
-    def get_encoding(self):
-        return self.encoding
+    def get_chan_est(self):
+        return self.chan_est
 
-    def set_encoding(self, encoding):
-        self.encoding = encoding
-        self._encoding_callback(self.encoding)
-        self.wifi_phy_hier_0.set_encoding(self.encoding)
+    def set_chan_est(self, chan_est):
+        self.chan_est = chan_est
+        self._chan_est_callback(self.chan_est)
+
+    def get_Debug(self):
+        return self.Debug
+
+    def set_Debug(self, Debug):
+        self.Debug = Debug
+        self._Debug_callback(self.Debug)
+        self.wireless_dump_wifi_dump_0.set_debug(self.Debug)
 
 
 
 
-def main(top_block_cls=WIIF_TX, options=None):
+def main(top_block_cls=WIFI_RX, options=None):
 
     qapp = Qt.QApplication(sys.argv)
 
