6d62ed737b82985d82205e37cd14d22ba58dfe7b
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-03-22 22:45:36 -0400

    udpate

--- agent/pack_data_to_bz2.py
+++ agent/pack_data_to_bz2.py
@@ -5,9 +5,12 @@ from datetime import datetime
 
 def main():
     try:
+        if not os.path.isfile("./wireless_data/patch_info.json"):
+            print("Not dataset to pack. Abort.")
+            return
         now = datetime.now()
         bz2_filename = now.strftime("%Y_%m_%d_%H_%M_%S")
-        folder_name = f"./wireless_data/{bz2_filename}"
+        folder_name = f"./wireless_data/Dataset_{bz2_filename}"
         os.mkdir(folder_name)
     
         with open("./wireless_data/patch_info.json", 'r') as f:
@@ -16,9 +19,11 @@ def main():
         for detail in all_file_details.keys():
             print(f"{detail = }")
             filename = all_file_details[detail]["Filename"]
-            os.system(f"cp ./wireless_data/{filename} {folder_name}")
+            os.system(f"mv ./wireless_data/{filename} {folder_name}")
+        os.system(f"mv ./wireless_data/patch_info.json {folder_name}")
     
-        # os.system("tar -cvjSf {folder_name}.tar.bz2 {folder_name}")
+        os.system(f"tar -cvjSf {folder_name}.tar.bz2 {folder_name}")
+        os.system(f"mv {folder_name}.tar.bz2 {folder_name}")
     except Exception as exp:
         e_type, e_obj, e_tb = sys.exc_info()
         print(f'Exception occurs: {exp}. At line {e_tb.tb_lineno}')
