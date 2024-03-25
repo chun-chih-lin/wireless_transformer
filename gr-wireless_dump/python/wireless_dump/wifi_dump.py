8526fcc46efc81349aebef4a54290a87145f10dc
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-03-25 13:20:35 -0400

    update

--- gr-wireless_dump/python/wireless_dump/wifi_dump.py
+++ gr-wireless_dump/python/wireless_dump/wifi_dump.py
@@ -153,9 +153,11 @@ class wifi_dump(gr.sync_block):
     def work(self, input_items, output_items):
         try:
             in0 = input_items[0]
-            tags = self.get_tags_in_window(0, 0, len(input_items[0]))
+            tags = self.get_tags_in_window(0, 0, len(input_items[0]), pmt.string_to_symbol("wifi_start"))
+            r_tags = self.get_tags_in_range(0, 0, len(input_items[0]), pmt.string_to_symbol("wifi_start"))
 
             print("----------------------------------")
+            print(f"{tags = }, {r_tags = }")
             print(f"{self.input_c = }, {len(in0) = }")
             self.input_c += 1
             
