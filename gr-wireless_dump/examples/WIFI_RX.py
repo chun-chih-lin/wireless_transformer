ba7d419cde8612885f72229dc4e4b2458c29f2fc
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-03-26 18:42:20 -0400

    update

--- gr-wireless_dump/examples/WIFI_RX.py
+++ gr-wireless_dump/examples/WIFI_RX.py
@@ -66,13 +66,15 @@ class WIFI_RX(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.window_size = window_size = 48
+        self.threshold = threshold = 0.01
         self.sync_length = sync_length = 320
         self.sdr_addr = sdr_addr = "addr=192.168.40.2"
         self.samp_rate = samp_rate = 20e6
+        self.record = record = 0
         self.pdu_length = pdu_length = 10
+        self.mod = mod = 0
         self.lo_offset = lo_offset = 0
         self.gain = gain = 0.75
-        self.freq = freq = 5170000000
         self.custom_freq = custom_freq = 2900e6
         self.chan_est = chan_est = 0
         self.Debug = Debug = 0
@@ -96,11 +98,22 @@ class WIFI_RX(gr.top_block, Qt.QWidget):
         self._samp_rate_combo_box.currentIndexChanged.connect(
             lambda i: self.set_samp_rate(self._samp_rate_options[i]))
         # Create the radio buttons
-        self.top_layout.addWidget(self._samp_rate_tool_bar)
+        self.top_grid_layout.addWidget(self._samp_rate_tool_bar, 0, 3, 1, 1)
+        for r in range(0, 1):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(3, 4):
+            self.top_grid_layout.setColumnStretch(c, 1)
         self._pdu_length_range = qtgui.Range(0, 1500, 1, 10, 200)
         self._pdu_length_win = qtgui.RangeWidget(self._pdu_length_range, self.set_pdu_length, "'pdu_length'", "counter_slider", int, QtCore.Qt.Horizontal)
-        self.top_grid_layout.addWidget(self._pdu_length_win, 3, 0, 1, 1)
+        self.top_grid_layout.addWidget(self._pdu_length_win, 3, 0, 1, 3)
         for r in range(3, 4):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 3):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self._mod_range = qtgui.Range(0, 7, 1, 0, 200)
+        self._mod_win = qtgui.RangeWidget(self._mod_range, self.set_mod, "'mod'", "counter_slider", int, QtCore.Qt.Horizontal)
+        self.top_grid_layout.addWidget(self._mod_win, 2, 0, 1, 1)
+        for r in range(2, 3):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
@@ -109,23 +122,36 @@ class WIFI_RX(gr.top_block, Qt.QWidget):
         # Create the labels list
         self._lo_offset_labels = ['0', '6000000.0', '11000000.0']
         # Create the combo box
-        self._lo_offset_tool_bar = Qt.QToolBar(self)
-        self._lo_offset_tool_bar.addWidget(Qt.QLabel("'lo_offset'" + ": "))
-        self._lo_offset_combo_box = Qt.QComboBox()
-        self._lo_offset_tool_bar.addWidget(self._lo_offset_combo_box)
-        for _label in self._lo_offset_labels: self._lo_offset_combo_box.addItem(_label)
-        self._lo_offset_callback = lambda i: Qt.QMetaObject.invokeMethod(self._lo_offset_combo_box, "setCurrentIndex", Qt.Q_ARG("int", self._lo_offset_options.index(i)))
-        self._lo_offset_callback(self.lo_offset)
-        self._lo_offset_combo_box.currentIndexChanged.connect(
-            lambda i: self.set_lo_offset(self._lo_offset_options[i]))
         # Create the radio buttons
-        self.top_layout.addWidget(self._lo_offset_tool_bar)
+        self._lo_offset_group_box = Qt.QGroupBox("'lo_offset'" + ": ")
+        self._lo_offset_box = Qt.QHBoxLayout()
+        class variable_chooser_button_group(Qt.QButtonGroup):
+            def __init__(self, parent=None):
+                Qt.QButtonGroup.__init__(self, parent)
+            @pyqtSlot(int)
+            def updateButtonChecked(self, button_id):
+                self.button(button_id).setChecked(True)
+        self._lo_offset_button_group = variable_chooser_button_group()
+        self._lo_offset_group_box.setLayout(self._lo_offset_box)
+        for i, _label in enumerate(self._lo_offset_labels):
+            radio_button = Qt.QRadioButton(_label)
+            self._lo_offset_box.addWidget(radio_button)
+            self._lo_offset_button_group.addButton(radio_button, i)
+        self._lo_offset_callback = lambda i: Qt.QMetaObject.invokeMethod(self._lo_offset_button_group, "updateButtonChecked", Qt.Q_ARG("int", self._lo_offset_options.index(i)))
+        self._lo_offset_callback(self.lo_offset)
+        self._lo_offset_button_group.buttonClicked[int].connect(
+            lambda i: self.set_lo_offset(self._lo_offset_options[i]))
+        self.top_layout.addWidget(self._lo_offset_group_box)
         self._gain_range = qtgui.Range(0, 1, 0.01, 0.75, 200)
         self._gain_win = qtgui.RangeWidget(self._gain_range, self.set_gain, "'gain'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._gain_win)
         self._custom_freq_range = qtgui.Range(2412e6, 5920e6, 5e6, 2900e6, 200)
         self._custom_freq_win = qtgui.RangeWidget(self._custom_freq_range, self.set_custom_freq, "'custom_freq'", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._custom_freq_win)
+        self.top_grid_layout.addWidget(self._custom_freq_win, 0, 0, 1, 3)
+        for r in range(0, 1):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 3):
+            self.top_grid_layout.setColumnStretch(c, 1)
         # Create the options list
         self._Debug_options = [0, 1]
         # Create the labels list
@@ -141,8 +167,12 @@ class WIFI_RX(gr.top_block, Qt.QWidget):
         self._Debug_combo_box.currentIndexChanged.connect(
             lambda i: self.set_Debug(self._Debug_options[i]))
         # Create the radio buttons
-        self.top_layout.addWidget(self._Debug_tool_bar)
-        self.wireless_dump_wifi_dump_0 = wireless_dump.wifi_dump(0, pdu_length, 0.01, Debug)
+        self.top_grid_layout.addWidget(self._Debug_tool_bar, 3, 3, 1, 1)
+        for r in range(3, 4):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(3, 4):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.wireless_dump_wifi_dump_0 = wireless_dump.wifi_dump(mod, pdu_length, 0.01, Debug)
         self.uhd_usrp_source_0 = uhd.usrp_source(
             ",".join(('', sdr_addr)),
             uhd.stream_args(
@@ -156,6 +186,20 @@ class WIFI_RX(gr.top_block, Qt.QWidget):
 
         self.uhd_usrp_source_0.set_center_freq(uhd.tune_request(custom_freq, rf_freq = custom_freq - lo_offset, rf_freq_policy=uhd.tune_request.POLICY_MANUAL), 0)
         self.uhd_usrp_source_0.set_normalized_gain(gain, 0)
+        self._threshold_range = qtgui.Range(0.0, 20, 0.01, 0.01, 200)
+        self._threshold_win = qtgui.RangeWidget(self._threshold_range, self.set_threshold, "Threshold for Saving", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_grid_layout.addWidget(self._threshold_win, 2, 1, 1, 2)
+        for r in range(2, 3):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(1, 3):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self._record_range = qtgui.Range(0, 1, 1, 0, 50)
+        self._record_win = qtgui.RangeWidget(self._record_range, self.set_record, "Record Packets", "slider", int, QtCore.Qt.Horizontal)
+        self.top_grid_layout.addWidget(self._record_win, 2, 3, 1, 1)
+        for r in range(2, 3):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(3, 4):
+            self.top_grid_layout.setColumnStretch(c, 1)
         self.qtgui_time_sink_x_0_0 = qtgui.time_sink_c(
             20480, #size
             samp_rate, #samp_rate
@@ -208,22 +252,6 @@ class WIFI_RX(gr.top_block, Qt.QWidget):
         self._qtgui_time_sink_x_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_0_win)
         self.ieee802_11_sync_short_0 = ieee802_11.sync_short(0.56, 2, False, False)
-        # Create the options list
-        self._freq_options = [2412000000.0, 2417000000.0, 2422000000.0, 2427000000.0, 2432000000.0, 2437000000.0, 2442000000.0, 2447000000.0, 2452000000.0, 2457000000.0, 2462000000.0, 2467000000.0, 2472000000.0, 2484000000.0, 5170000000.0, 5180000000.0, 5190000000.0, 5200000000.0, 5210000000.0, 5220000000.0, 5230000000.0, 5240000000.0, 5250000000.0, 5260000000.0, 5270000000.0, 5280000000.0, 5290000000.0, 5300000000.0, 5310000000.0, 5320000000.0, 5500000000.0, 5510000000.0, 5520000000.0, 5530000000.0, 5540000000.0, 5550000000.0, 5560000000.0, 5570000000.0, 5580000000.0, 5590000000.0, 5600000000.0, 5610000000.0, 5620000000.0, 5630000000.0, 5640000000.0, 5660000000.0, 5670000000.0, 5680000000.0, 5690000000.0, 5700000000.0, 5710000000.0, 5720000000.0, 5745000000.0, 5755000000.0, 5765000000.0, 5775000000.0, 5785000000.0, 5795000000.0, 5805000000.0, 5825000000.0, 5860000000.0, 5870000000.0, 5880000000.0, 5890000000.0, 5900000000.0, 5910000000.0, 5920000000.0]
-        # Create the labels list
-        self._freq_labels = ['  1 | 2412.0 | 11g', '  2 | 2417.0 | 11g', '  3 | 2422.0 | 11g', '  4 | 2427.0 | 11g', '  5 | 2432.0 | 11g', '  6 | 2437.0 | 11g', '  7 | 2442.0 | 11g', '  8 | 2447.0 | 11g', '  9 | 2452.0 | 11g', ' 10 | 2457.0 | 11g', ' 11 | 2462.0 | 11g', ' 12 | 2467.0 | 11g', ' 13 | 2472.0 | 11g', ' 14 | 2484.0 | 11g', ' 34 | 5170.0 | 11a', ' 36 | 5180.0 | 11a', ' 38 | 5190.0 | 11a', ' 40 | 5200.0 | 11a', ' 42 | 5210.0 | 11a', ' 44 | 5220.0 | 11a', ' 46 | 5230.0 | 11a', ' 48 | 5240.0 | 11a', ' 50 | 5250.0 | 11a', ' 52 | 5260.0 | 11a', ' 54 | 5270.0 | 11a', ' 56 | 5280.0 | 11a', ' 58 | 5290.0 | 11a', ' 60 | 5300.0 | 11a', ' 62 | 5310.0 | 11a', ' 64 | 5320.0 | 11a', '100 | 5500.0 | 11a', '102 | 5510.0 | 11a', '104 | 5520.0 | 11a', '106 | 5530.0 | 11a', '108 | 5540.0 | 11a', '110 | 5550.0 | 11a', '112 | 5560.0 | 11a', '114 | 5570.0 | 11a', '116 | 5580.0 | 11a', '118 | 5590.0 | 11a', '120 | 5600.0 | 11a', '122 | 5610.0 | 11a', '124 | 5620.0 | 11a', '126 | 5630.0 | 11a', '128 | 5640.0 | 11a', '132 | 5660.0 | 11a', '134 | 5670.0 | 11a', '136 | 5680.0 | 11a', '138 | 5690.0 | 11a', '140 | 5700.0 | 11a', '142 | 5710.0 | 11a', '144 | 5720.0 | 11a', '149 | 5745.0 | 11a (SRD)', '151 | 5755.0 | 11a (SRD)', '153 | 5765.0 | 11a (SRD)', '155 | 5775.0 | 11a (SRD)', '157 | 5785.0 | 11a (SRD)', '159 | 5795.0 | 11a (SRD)', '161 | 5805.0 | 11a (SRD)', '165 | 5825.0 | 11a (SRD)', '172 | 5860.0 | 11p', '174 | 5870.0 | 11p', '176 | 5880.0 | 11p', '178 | 5890.0 | 11p', '180 | 5900.0 | 11p', '182 | 5910.0 | 11p', '184 | 5920.0 | 11p']
-        # Create the combo box
-        self._freq_tool_bar = Qt.QToolBar(self)
-        self._freq_tool_bar.addWidget(Qt.QLabel("'freq'" + ": "))
-        self._freq_combo_box = Qt.QComboBox()
-        self._freq_tool_bar.addWidget(self._freq_combo_box)
-        for _label in self._freq_labels: self._freq_combo_box.addItem(_label)
-        self._freq_callback = lambda i: Qt.QMetaObject.invokeMethod(self._freq_combo_box, "setCurrentIndex", Qt.Q_ARG("int", self._freq_options.index(i)))
-        self._freq_callback(self.freq)
-        self._freq_combo_box.currentIndexChanged.connect(
-            lambda i: self.set_freq(self._freq_options[i]))
-        # Create the radio buttons
-        self.top_layout.addWidget(self._freq_tool_bar)
         # Create the options list
         self._chan_est_options = [0, 1, 2, 3]
         # Create the labels list
@@ -295,6 +323,12 @@ class WIFI_RX(gr.top_block, Qt.QWidget):
         self.blocks_moving_average_xx_0.set_length_and_scale((self.window_size  + 16), 1)
         self.blocks_moving_average_xx_1.set_length_and_scale(self.window_size, 1)
 
+    def get_threshold(self):
+        return self.threshold
+
+    def set_threshold(self, threshold):
+        self.threshold = threshold
+
     def get_sync_length(self):
         return self.sync_length
 
@@ -316,12 +350,25 @@ class WIFI_RX(gr.top_block, Qt.QWidget):
         self.qtgui_time_sink_x_0_0.set_samp_rate(self.samp_rate)
         self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
 
+    def get_record(self):
+        return self.record
+
+    def set_record(self, record):
+        self.record = record
+
     def get_pdu_length(self):
         return self.pdu_length
 
     def set_pdu_length(self, pdu_length):
         self.pdu_length = pdu_length
         self.wireless_dump_wifi_dump_0.set_pdu_len(self.pdu_length)
+
+    def get_mod(self):
+        return self.mod
+
+    def set_mod(self, mod):
+        self.mod = mod
+        self.wireless_dump_wifi_dump_0.set_modulation(self.mod)
 
     def get_lo_offset(self):
         return self.lo_offset
@@ -337,13 +384,6 @@ class WIFI_RX(gr.top_block, Qt.QWidget):
     def set_gain(self, gain):
         self.gain = gain
         self.uhd_usrp_source_0.set_normalized_gain(self.gain, 0)
-
-    def get_freq(self):
-        return self.freq
-
-    def set_freq(self, freq):
-        self.freq = freq
-        self._freq_callback(self.freq)
 
     def get_custom_freq(self):
         return self.custom_freq
