780c46427df8a752f717e34d94da2439a88a1e5d
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-03-26 15:25:31 -0400

    updte

--- agent/agent_service_start.sh
+++ agent/agent_service_start.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+
+
+if [[ $1 = "1" ]]
+then
+    clear
+    echo "Restarting the service..."
+    ./agent_service_stop.sh
+fi
+
+echo "Trying starting the service..."
+
+IS_RUNNING=`redis-cli -h localhost -p 6379 GET AGENT:COLLECT`
+if [[ $IS_RUNNING = "Running" ]]
+then
+    echo "It is already running. Stop first. Abort."
+    exit 0
+fi
+
+echo "Starting the service..."
 # Import default keys into redis db
 redis-cli -h localhost -p 6379 SET AGENT:COLLECT Starting
 
