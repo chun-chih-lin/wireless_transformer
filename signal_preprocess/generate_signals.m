111a2eb39db5aa35e257e50b681f212427aae782
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-01 18:48:00 -0400

    update

--- signal_preprocess/generate_signals.m
+++ signal_preprocess/generate_signals.m
@@ -2,9 +2,29 @@ clc
 clear
 close all
 %%
-% WBFM; BPSK; QPSK; 8PSK; 16QAM; 64QAM; 4PAM; CPFSK; AM-SSB; SM-DSB
+% Discrete sample:
+%   BPSK; QPSK; 8PSK; 16QAM; 64QAM; 4PAM; CPFSK
+% Continue sample:
+%   WBFM; AM-SSB; SM-DSB
+
+%% Wi-Fi
+
 
 %% WBFM
+% http://hyperphysics.phy-astr.gsu.edu/hbase/Audio/radio.html
+fs = 1e3;           % Sample Frequency
+fc = 200;           % Carrier Frequency 88.1 MHz - 108.1 MHz
+fDev = 50;          % Frequency Deviation (Hz)/Modulation index
+
+t = (0:1/fs:.1);
+continue_signal = sin(2*pi*30*t) + 2*sin(2*pi*60*t);
+
+fm_signal = fmmod(continue_signal, fc, fs, fDev);
+fm_fig = figure();
+plot(t, fm_signal)
+hold on
+plot(t, continue_signal)
+legend("Modulated Signal", "Original Signal")
 
 
 %% 4PAM
@@ -33,18 +53,35 @@ plot(imag(cpfsk_signal))
 
 
 %% AM-SSB; AM-DSB
-fs = 100;
-fc = 10;
-t = (0:1/fs:1);
-x = sin(2*pi*t);
+% http://hyperphysics.phy-astr.gsu.edu/hbase/Audio/radio.html
+fs = 44.1e3;           % Sample Frequency
+fc = 550e3;           % Carrier Frequency 540k - 1600k Hz
+t = (0:1/fs:.01);
+continue_signal = sin(2*pi*30*t) + 2*sin(2*pi*60*t);
 
-am_dsb = ammod(x, fc, fs);
-am_ssb = ssbmod(x, fc, fs);
+s_t = continue_signal;
+h_t = imag(hilbert(continue_signal));
+s_lsb = s_t.*cos(2*pi*fc*t) - h_t.*sin(2*pi*fc*t);
+s_usb = s_t.*cos(2*pi*fc*t) + h_t.*sin(2*pi*fc*t);
+s_dsb = s_lsb + s_usb;
 
 am_fig = figure();
-plot(am_dsb)
+plot(real(s_lsb))
 hold on
-plot(am_ssb)
+plot(real(s_usb))
+hold on
+plot(s_dsb)
+legend("AM-LowerSide", "AM-UpperSide", "AM-DSB")
+
+
+% am_dsb = ammod(continue_signal, fc, fs);
+% am_ssb = ssbmod(continue_signal, fc, fs);
+% 
+% am_fig = figure();
+% plot(am_dsb)
+% hold on
+% plot(am_ssb)
+% legend("AM-DSB", "AM-SSB")
 
 
 %%
--- signal_preprocess/preprocess.m
+++ signal_preprocess/preprocess.m
@@ -7,12 +7,51 @@ dft_matrix = dftmtx(64);
 
 save_fig_folder = "./results/";
 
+
+sync_long = get_sync_long();
+
+cfg = wlanHTConfig('ChannelBandwidth', 'CBW20');
+HTLTF = wlanHTLTF(cfg);
+HTSTF = wlanHTSTF(cfg);
+
+sync_long_dot_ret = row_x_matrix(HTLTF(1:64).', dft_matrix);
+sync_short_dot_ret = row_x_matrix(HTSTF(1:64).', dft_matrix);
+
+figure();
+subplot(2, 2, 1)
+plot(real(HTLTF), 'b')
+hold on
+plot(imag(HTLTF), 'r')
+xlim([1, 64])
+title("Long-Training-Field")
+
+subplot(2, 2, 2)
+plot(real(HTSTF), 'b')
+hold on
+plot(imag(HTSTF), 'r')
+xlim([1, 64])
+title("Short-Training-Field")
+
+subplot(2, 2, 3)
+plot(real(sync_long_dot_ret), 'b')
+hold on
+plot(imag(sync_long_dot_ret), 'r')
+xlim([1, 64])
+title("DFT Long-Training-Field")
+
+subplot(2, 2, 4)
+plot(real(sync_short_dot_ret), 'b')
+hold on
+plot(imag(sync_short_dot_ret), 'r')
+xlim([1, 64])
+title("DFT Short-Training-Field")
+
 for e = 0:7
     [dataset, len] = get_signal(e, 128);
 
     n_odfm_symbol = floor(len/80);
 
-    for i_symbol = 9:n_odfm_symbol
+    for i_symbol = 1:n_odfm_symbol
         smp_s = (i_symbol-1)*80+1;
         smp_e = i_symbol*80;
         
@@ -86,7 +125,7 @@ for e = 0:7
         hold on
         plot(symbol_sample(2, :), 'r-')
         title("Original Signal")
-        xlim([1, 80])
+        xlim([1, 64])
 
         subplot(2, 2, 4)
         plot(real(dot_ret_cyclic(show_cyclic, :)), 'b-')
@@ -94,6 +133,7 @@ for e = 0:7
         title("DFT Result")
         plot(imag(dot_ret_cyclic(show_cyclic, :)), 'r-')
         xlim([1, 64])
+        ylim([-10, 10])
 
         subplot(2, 2, [1, 3])
         plot(real(dot_ret_cyclic(show_cyclic, :)), imag(dot_ret_cyclic(show_cyclic, :)), 'bo')
@@ -109,7 +149,7 @@ for e = 0:7
         saveas(fig, strcat(save_fig_folder, filename))
         break
     end
-    % break
+    break
 end
 
 
@@ -140,7 +180,30 @@ function [d, len] = get_signal(e, pdu_len)
     d = d(:, 1:len);
 end
 
-
+function sync_long = get_sync_long()
+    sync_long = [-0.0455 - 1.0679*1j, 0.3528 - 0.9865*1j, 0.8594 + 0.7348*1j, ...
+                 0.1874 + 0.2475*1j, 0.5309 - 0.7784*1j, -1.0218 - 0.4897*1j, ...
+                 -0.3401 - 0.9423*1j, 0.8657 - 0.2298*1j, 0.4734 + 0.0362*1j, ...
+                 0.0088 - 1.0207*1j, -1.2142 - 0.4205*1j, 0.2172 - 0.5195*1j, ...
+                 0.5207 - 0.1326*1j, -0.1995 + 1.4259*1j, 1.0583 - 0.0363*1j, ...
+                 0.5547 - 0.5547*1j, 0.3277 + 0.8728*1j, -0.5077 + 0.3488*1j, ...
+                 -1.1650 + 0.5789*1j, 0.7297 + 0.8197*1j, 0.6173 + 0.1253*1j, ...
+                 -0.5353 + 0.7214*1j, -0.5011 - 0.1935*1j, -0.3110 - 1.3392*1j, ...
+                 -1.0818 - 0.1470*1j, -1.1300 - 0.1820*1j, 0.6663 - 0.6571*1j, ...
+                 -0.0249 + 0.4773*1j, -0.8155 + 1.0218*1j, 0.8140 + 0.9396*1j, ...
+                 0.1090 + 0.8662*1j, -1.3868 - 0.0000*1j, 0.1090 - 0.8662*1j, ...
+                 0.8140 - 0.9396*1j, -0.8155 - 1.0218*1j, -0.0249 - 0.4773*1j, ...
+                 0.6663 + 0.6571*1j, -1.1300 + 0.1820*1j, -1.0818 + 0.1470*1j, ...
+                 -0.3110 + 1.3392*1j, -0.5011 + 0.1935*1j, -0.5353 - 0.7214*1j, ...
+                 0.6173 - 0.1253*1j, 0.7297 - 0.8197*1j, -1.1650 - 0.5789*1j, ...
+                 -0.5077 - 0.3488*1j, 0.3277 - 0.8728*1j, 0.5547 + 0.5547*1j, ...
+                 1.0583 + 0.0363*1j, -0.1995 - 1.4259*1j, 0.5207 + 0.1326*1j, ...
+                 0.2172 + 0.5195*1j, -1.2142 + 0.4205*1j, 0.0088 + 1.0207*1j, ...
+                 0.4734 - 0.0362*1j, 0.8657 + 0.2298*1j, -0.3401 + 0.9423*1j, ...
+                 -1.0218 + 0.4897*1j, 0.5309 + 0.7784*1j, 0.1874 - 0.2475*1j, ...
+                 0.8594 - 0.7348*1j, 0.3528 + 0.9865*1j, -0.0455 + 1.0679*1j, ...
+                 1.3868 - 0.0000*1j];
+end
 
 
 
