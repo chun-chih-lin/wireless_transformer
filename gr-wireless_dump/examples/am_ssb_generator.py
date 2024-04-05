4371281f67cf81f96b958671e30b8c65be1a546d
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-05 00:31:46 -0400

    update

--- gr-wireless_dump/examples/am_ssb_generator.py
+++ gr-wireless_dump/examples/am_ssb_generator.py
@@ -28,7 +28,7 @@ import sip
 
 
 
-class am_generator(gr.top_block, Qt.QWidget):
+class am_ssb_generator(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
@@ -51,7 +51,7 @@ class am_generator(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "am_generator")
+        self.settings = Qt.QSettings("GNU Radio", "am_ssb_generator")
 
         try:
             geometry = self.settings.value("geometry")
@@ -63,12 +63,9 @@ class am_generator(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.ratio = ratio = 50
-        self.audio_source_sampling_rate = audio_source_sampling_rate = 44.1e3
-        self.out_samp_rate = out_samp_rate = audio_source_sampling_rate*ratio
+        self.samp_rate = samp_rate = 32000
         self.input_gain = input_gain = 2
-        self.am_sin_freq = am_sin_freq = 100e3
-        self.am_sin_amp = am_sin_amp = 1
+        self.audio_source_sampling_rate = audio_source_sampling_rate = 44.1e3
 
         ##################################################
         # Blocks
@@ -77,30 +74,35 @@ class am_generator(gr.top_block, Qt.QWidget):
         self._input_gain_range = qtgui.Range(0, 10, 0.1, 2, 200)
         self._input_gain_win = qtgui.RangeWidget(self._input_gain_range, self.set_input_gain, "'input_gain'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._input_gain_win)
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=ratio,
-                decimation=1,
+        self.rational_resampler_xxx_0_0_0 = filter.rational_resampler_ccf(
+                interpolation=1,
+                decimation=5,
                 taps=[],
                 fractional_bw=0)
-        self.qtgui_time_sink_x_1 = qtgui.time_sink_c(
-            10240, #size
-            out_samp_rate, #samp_rate
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=441,
+                decimation=500,
+                taps=[],
+                fractional_bw=0)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
+            1024, #size
+            samp_rate, #samp_rate
             "", #name
             1, #number of inputs
             None # parent
         )
-        self.qtgui_time_sink_x_1.set_update_time(0.10)
-        self.qtgui_time_sink_x_1.set_y_axis(-1, 1)
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
 
-        self.qtgui_time_sink_x_1.set_y_label('Amplitude', "")
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
 
-        self.qtgui_time_sink_x_1.enable_tags(True)
-        self.qtgui_time_sink_x_1.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_1.enable_autoscale(False)
-        self.qtgui_time_sink_x_1.enable_grid(False)
-        self.qtgui_time_sink_x_1.enable_axis_labels(True)
-        self.qtgui_time_sink_x_1.enable_control_panel(False)
-        self.qtgui_time_sink_x_1.enable_stem_plot(False)
+        self.qtgui_time_sink_x_0.enable_tags(True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
 
 
         labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
@@ -120,57 +122,9 @@ class am_generator(gr.top_block, Qt.QWidget):
         for i in range(2):
             if len(labels[i]) == 0:
                 if (i % 2 == 0):
-                    self.qtgui_time_sink_x_1.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
                 else:
-                    self.qtgui_time_sink_x_1.set_line_label(i, "Im{{Data {0}}}".format(i/2))
-            else:
-                self.qtgui_time_sink_x_1.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_1.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_1.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_1.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_1.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_1.set_line_alpha(i, alphas[i])
-
-        self._qtgui_time_sink_x_1_win = sip.wrapinstance(self.qtgui_time_sink_x_1.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_1_win)
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-            2048, #size
-            audio_source_sampling_rate, #samp_rate
-            "", #name
-            2, #number of inputs
-            None # parent
-        )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
-
-        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
-
-        self.qtgui_time_sink_x_0.enable_tags(True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(False)
-        self.qtgui_time_sink_x_0.enable_grid(False)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
-        self.qtgui_time_sink_x_0.enable_stem_plot(False)
-
-
-        labels = ['Modulated', 'Baseband', 'Signal 3', 'Signal 4', 'Signal 5',
-            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ['blue', 'red', 'green', 'black', 'cyan',
-            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-        styles = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-            -1, -1, -1, -1, -1]
-
-
-        for i in range(2):
-            if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
             else:
                 self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
             self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
@@ -181,11 +135,11 @@ class am_generator(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_f(
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
             0, #fc
-            audio_source_sampling_rate, #bw
+            samp_rate, #bw
             "", #name
             1,
             None # parent
@@ -202,7 +156,6 @@ class am_generator(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_0.set_fft_window_normalized(False)
 
 
-        self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
 
         labels = ['', '', '', '', '',
             '', '', '', '', '']
@@ -224,65 +177,56 @@ class am_generator(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.hilbert_fc_0 = filter.hilbert_fc(401, window.WIN_HAMMING, 6.76)
         self.blocks_wavfile_source_0 = blocks.wavfile_source('/home/ubuntu2204/Desktop/wireless_transformer/gr-wireless_dump/examples/Rick Astley  Never Gonna Give You Up.wav', True)
-        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, audio_source_sampling_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * audio_source_sampling_rate) if "auto" == "time" else int(0.1), 1) )
-        self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
+        self.blocks_multiply_xx_0_0_0 = blocks.multiply_vcc(1)
+        self.blocks_multiply_const_vxx_0_0_0 = blocks.multiply_const_cc(1000)
         self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_ff(input_gain)
-        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
         self.blocks_add_const_vxx_0_0 = blocks.add_const_ff(1)
-        self.analog_sig_source_x_0 = analog.sig_source_f(audio_source_sampling_rate, analog.GR_SIN_WAVE, am_sin_freq, am_sin_amp, 0, 0)
-        self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 0)
+        self.band_pass_filter_0_0 = filter.fir_filter_ccf(
+            1,
+            firdes.band_pass(
+                1,
+                50e3,
+                300,
+                3000,
+                100,
+                window.WIN_HAMMING,
+                6.76))
+        self.analog_sig_source_x_0_0 = analog.sig_source_c(50e3, analog.GR_COS_WAVE, 10e3, 1, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_const_source_x_0, 0), (self.blocks_float_to_complex_0, 1))
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 0))
-        self.connect((self.blocks_add_const_vxx_0_0, 0), (self.blocks_float_to_complex_0, 0))
-        self.connect((self.blocks_add_const_vxx_0_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.blocks_add_const_vxx_0_0, 0), (self.qtgui_time_sink_x_0, 1))
-        self.connect((self.blocks_float_to_complex_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.blocks_add_const_vxx_0_0, 0))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_throttle2_0, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0_0_0, 1))
+        self.connect((self.band_pass_filter_0_0, 0), (self.blocks_multiply_xx_0_0_0, 0))
+        self.connect((self.blocks_add_const_vxx_0_0, 0), (self.hilbert_fc_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0_0_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0_0_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_multiply_xx_0_0_0, 0), (self.rational_resampler_xxx_0_0_0, 0))
         self.connect((self.blocks_wavfile_source_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.qtgui_time_sink_x_1, 0))
+        self.connect((self.hilbert_fc_0, 0), (self.band_pass_filter_0_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_add_const_vxx_0_0, 0))
+        self.connect((self.rational_resampler_xxx_0_0_0, 0), (self.blocks_multiply_const_vxx_0_0_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "am_generator")
+        self.settings = Qt.QSettings("GNU Radio", "am_ssb_generator")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
 
         event.accept()
 
-    def get_ratio(self):
-        return self.ratio
+    def get_samp_rate(self):
+        return self.samp_rate
 
-    def set_ratio(self, ratio):
-        self.ratio = ratio
-        self.set_out_samp_rate(self.audio_source_sampling_rate*self.ratio)
-
-    def get_audio_source_sampling_rate(self):
-        return self.audio_source_sampling_rate
-
-    def set_audio_source_sampling_rate(self, audio_source_sampling_rate):
-        self.audio_source_sampling_rate = audio_source_sampling_rate
-        self.set_out_samp_rate(self.audio_source_sampling_rate*self.ratio)
-        self.analog_sig_source_x_0.set_sampling_freq(self.audio_source_sampling_rate)
-        self.blocks_throttle2_0.set_sample_rate(self.audio_source_sampling_rate)
-        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.audio_source_sampling_rate)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.audio_source_sampling_rate)
-
-    def get_out_samp_rate(self):
-        return self.out_samp_rate
-
-    def set_out_samp_rate(self, out_samp_rate):
-        self.out_samp_rate = out_samp_rate
-        self.qtgui_time_sink_x_1.set_samp_rate(self.out_samp_rate)
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
     def get_input_gain(self):
         return self.input_gain
@@ -291,24 +235,16 @@ class am_generator(gr.top_block, Qt.QWidget):
         self.input_gain = input_gain
         self.blocks_multiply_const_vxx_0_0.set_k(self.input_gain)
 
-    def get_am_sin_freq(self):
-        return self.am_sin_freq
+    def get_audio_source_sampling_rate(self):
+        return self.audio_source_sampling_rate
 
-    def set_am_sin_freq(self, am_sin_freq):
-        self.am_sin_freq = am_sin_freq
-        self.analog_sig_source_x_0.set_frequency(self.am_sin_freq)
-
-    def get_am_sin_amp(self):
-        return self.am_sin_amp
-
-    def set_am_sin_amp(self, am_sin_amp):
-        self.am_sin_amp = am_sin_amp
-        self.analog_sig_source_x_0.set_amplitude(self.am_sin_amp)
+    def set_audio_source_sampling_rate(self, audio_source_sampling_rate):
+        self.audio_source_sampling_rate = audio_source_sampling_rate
 
 
 
 
-def main(top_block_cls=am_generator, options=None):
+def main(top_block_cls=am_ssb_generator, options=None):
 
     qapp = Qt.QApplication(sys.argv)
 
