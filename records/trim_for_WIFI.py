b847cbe85abffddde82ad49a428459b540b87f42
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-17 16:27:27 -0400

    update

--- records/trim_for_WIFI.py
+++ records/trim_for_WIFI.py
@@ -52,9 +52,9 @@ def trim(data, pkt_len, mod):
     sec_threshold = 0.00038
     pkt_e_threshold_list = {
         "BPSK": 0.0025,
-        "QPSK": 0.00015,
-        "16QAM": 0.00015,
-        "64QAM": 0.00015
+        "QPSK": 0.0015,
+        "16QAM": 0.0015,
+        "64QAM": 0.0015
     }
 
     pkt_energy_threshold = pkt_e_threshold_list[mod]
