9ffdbc875e3b5b296586a45f42f5c7d2c92307c5
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-17 16:09:51 -0400

    update

--- records/trim_for_WIFI.py
+++ records/trim_for_WIFI.py
@@ -167,8 +167,8 @@ def main(args):
         s = None
         e = None
 
-        s = 500_000
-        e = 1_000_000
+        s = 1_000_000
+        e = 10_000_000
 
         if os.path.isfile(dat_filename):
             print("is file")
