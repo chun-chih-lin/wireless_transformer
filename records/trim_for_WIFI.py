e245bcff1a9d198a55f0f3e486300742e827fa5c
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-18 23:18:06 -0400

    update

--- records/trim_for_WIFI.py
+++ records/trim_for_WIFI.py
@@ -77,10 +77,10 @@ def trim(args, data, pkt_len, tx_pwr, mod):
             "e_threshold": 0.0025
         },
         "-10": {
-            "BPSK": 0.0025,
-            "QPSK": 0.0020,
-            "16QAM": 0.0015,
-            "64QAM": 0.0015,
+            "BPSK": 0.001,
+            "QPSK": 0.001,
+            "16QAM": 0.007,
+            "64QAM": 0.01,
             "e_threshold": 0.0025
         },
         "-15": {
