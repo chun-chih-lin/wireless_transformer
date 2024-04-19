b51e46a7bac2769a452d6f7d8d6ea4519f937f53
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-18 22:50:31 -0400

    update

--- records/combine_protocols.py
+++ records/combine_protocols.py
@@ -13,6 +13,7 @@ parser.add_argument('-i', help='Insepect the first 1_000_000 samples', default=F
 args = parser.parse_args()
 
 mod_list = ["Trimmed-WIFI-BPSK", "Trimmed-WIFI-QPSK", "Trimmed-WIFI-16QAM", "Trimmed-WIFI-64QAM", "ZIGBEE-OQPSK", "BT-GFSK-LE1M", "BT-GFSK-LE2M", "BT-GFSK-S2Coding", "BT-GFSK-S8Coding"]
+mod_real_name_list = ["WIFI-BPSK", "WIFI-QPSK", "WIFI-16QAM", "WIFI-64QAM", "ZIGBEE-OQPSK", "BT-GFSK-LE1M", "BT-GFSK-LE2M", "BT-GFSK-S2Coding", "BT-GFSK-S8Coding"]
 mod_idx = [x for x in range(len(mod_list))]
 mod_len = [1280, 880, 640, 560, 128, 128, 128, 128, 128]
 
@@ -36,12 +37,12 @@ def is_all_file_exist():
 def main():
     print(f"{args = }")
 
+    dataset_prefix = args.split("/")[1]
     all_exist, fail_list = is_all_file_exist()
     print(f"{all_exist = }, {fail_list = }")
-    
-    
 
-    for mod, mod_i, mod_l in zip(mod_list, mod_idx, mod_len):
+    dataset_dict = {}
+    for mod, mod_name, mod_i, mod_l in zip(mod_list, mod_real_name_list, mod_idx, mod_len):
         print("------------------------")
         src_filename = f"{args.s}{mod}{args.p}dat"
         record_data = np.fromfile(open(src_filename), dtype=np.complex64)
@@ -51,9 +52,23 @@ def main():
         input_len = int(record_data.shape[0]/mod_l)
 
         _X_c = record_data.reshape((input_len, mod_l))
-        print(f"{_X_c.shape = }")
+        _Y = np.array([mod_i for x in range(_X_c.shape[0])])
+
+        _X_i = np.expand_dims(np.real(_X_c), axis=1)
+        _X_q = np.expand_dims(np.imag(_X_c), axis=1)
+
+        _X = np.concatenate((_X_i, _X_q), axis=1)
 
 
+        dataset_dict[mod_name] = {
+            "X": _X,
+            "Y": _Y
+            }
+
+
+    print(f"{dataset_dict.keys() = }")
+    save_filename = f"{args.s}{dataset_prefix}-protocol{args.p}pkl"
+    print(f"Save to {save_filename}")
     pass
 
 if __name__ == "__main__":
