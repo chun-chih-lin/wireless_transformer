304bf6ae2c445461e89317499f6e4cff79bd059f
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-21 17:21:43 -0400

    update

--- records/frequency_extraction.py
+++ records/frequency_extraction.py
@@ -5,8 +5,26 @@ def get_dft_matrix(N=64, M=1):
     dft_mtx = np.expand_dims(np.fft.fft(np.eye(N)), axis=0)
     return np.repeat(dft_mtx, M, axis=0)
 
-def create_shift_sub_ary():
-    pass
+def dft(input_ary, batch_size=10_000):
+    n_batch = int(input_ary.shape[0]/batch_size) + 1
+
+    dft_mtx = get_dft_matrix(N=64, M=batch_size)
+
+    dft_ret = None
+    for n_b in range(n_batch):
+        print(f"  Batch [{n_b}/{n_batch}]...")
+        if (n_b+1)*batch_size <= input_ary.shape[0]:
+            batch_ary = input_ary[n_b*batch_size:(n_b+1)*batch_size, :]
+        else:
+            batch_ary = input_ary[n_b*batch_size:, :]
+            dft_mtx = get_dft_matrix(N=64, M=input_ary.shape[0]-n_b*batch_size)
+
+        sub_dft_ret = np.matmul(batch_ary, dft_mtx)
+        if dft_ret is None:
+            dft_ret = sub_dft_ret
+        else:
+            dft_ret = np.concatenate((dft_ret, sub_dft_ret), axis=0)
+    return dft_ret
 
 def frequency_extraction(input_ary, indent=8):
     if len(input_ary.shape) == 2:
@@ -17,7 +35,7 @@ def frequency_extraction(input_ary, indent=8):
     num_pkt = complex_input.shape[0]
     sub_ary_len = int(complex_input.shape[1]/2)
 
-    dft_mtx = get_dft_matrix(N=sub_ary_len, M=num_pkt)
+    # dft_mtx = get_dft_matrix(N=sub_ary_len, M=num_pkt)
 
     time_indent_len = int(sub_ary_len/indent)
 
@@ -25,7 +43,8 @@ def frequency_extraction(input_ary, indent=8):
 
     for i, idx in enumerate(range(0, sub_ary_len, indent)):
         sub_ary = np.expand_dims(complex_input[:, idx:idx+sub_ary_len], axis=1)
-        dft_mtx_ret = np.matmul(sub_ary, dft_mtx)
+        dft_mtx_ret = dft(sub_ary)
+        # dft_mtx_ret = np.matmul(sub_ary, dft_mtx)
         ttl_dft_mtx_ret[:, i, :] = np.fft.fftshift(np.squeeze(dft_mtx_ret), axes=(1, ))
 
     return ttl_dft_mtx_ret
