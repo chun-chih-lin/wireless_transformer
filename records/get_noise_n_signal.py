6616f1968723ce117c787827a7585ec33f645bc4
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-22 22:22:22 -0400

    update

--- records/get_noise_n_signal.py
+++ records/get_noise_n_signal.py
@@ -5,7 +5,7 @@ import _pickle as pickle
 import argparse
 parser = argparse.ArgumentParser(description='save torch experience file to npy.')
 parser.add_argument('-s', help='source folder', required=True)
-parser.add_argument('-n', help='source pickle filename', required=True)
+parser.add_argument('-p', help='filename pattern', required=True)
 args = parser.parse_args()
 
 ret = os.system('clear')
@@ -18,12 +18,23 @@ HAS_NOISE_LIST = ["WIFI-BPSK", "WIFI-QPSK", "WIFI-16QAM", "WIFI-64QAM"]
 # ======================================================
 def load_pickle(filename):
     if not os.path.isfile(filename):
-        print(f"{filname} is not exist.")
+        print(f"{filename} is not exist.")
         exit()
     with open(filename, 'rb') as f:
         data = pickle.load(f, encoding='latin1')
     return data
     pass
+
+def get_filenames_under_folder():
+    filename_list = []
+    for mod in MOD_LIST:
+        filename = f"{mod}{args.p}dat"
+        full_filename = f"{args.s}{filename}"
+        if os.path.isfile(full_filename):
+            filename_list.append(filename)
+        else:
+            print(f"{filename} is not a file.")
+    return filename_list
 
 def get_noise_n_signal(ary, spl_size=500):
     sig = np.zeros((spl_size, ))
@@ -35,11 +46,13 @@ def get_noise_n_signal(ary, spl_size=500):
 
 # ======================================================
 def main():
-    data = load_pickle(f"{args.s}{args.n}")
+    
     print(data.keys())
-    for mod in MOD_LIST:
+    file_list = get_filenames_under_folder()
+    for filename in file_list:
         print('-'*20)
-        print(mod)
+        print(f"{filename = }")
+        data = load_pickle(f"{filename}")
         sig, noise = get_noise_n_signal(data[mod]["X"])
 
     pass
