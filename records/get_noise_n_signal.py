cefb8de44ddf6af0d92df43dc337a30132d1bab8
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-23 00:11:33 -0400

    update

--- records/get_noise_n_signal.py
+++ records/get_noise_n_signal.py
@@ -101,10 +101,10 @@ def main():
             # record_data = record_data[15_000:50_000]
 
             # EIB_outdoor: 30_000:70_000
-            record_data = record_data[30_000:70_000]
+            # record_data = record_data[30_000:70_000]
 
             # EIB_room_328_to_hallway: 20_000:50_000
-            # record_data = record_data[20_000:50_000]
+            record_data = record_data[20_000:50_000]
 
             if filename.find("WIFI") > 0:
                 has_noise = True
