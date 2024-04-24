47c0a4d9317c298a77e889b5e6b13b2365b9bff2
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-24 19:41:11 -0400

    update

--- records/get_iot_packet.py
+++ records/get_iot_packet.py
@@ -6,14 +6,17 @@ import argparse
 parser = argparse.ArgumentParser(description='save torch experience file to npy.')
 parser.add_argument('-s', help='source folder.', required=True)
 parser.add_argument('-p', help='source pattern.', required=True)
+parser.add_argument('-i', help='inspect mode.', default=False, action='store_true')
 args = parser.parse_args()
 
 
+INSPECT = args.i
 MOD_LIST = ["WIFI-BPSK", "WIFI-QPSK", "WIFI-16QAM", "WIFI-64QAM", "ZIGBEE-OQPSK", "BT-GFSK-LE1M", "BT-GFSK-LE2M", "BT-GFSK-S2Coding", "BT-GFSK-S2Coding"]
 
 if os.system("clear") != 0:
     os.system("cls")
 
+# ==========================================================
 def load_dat_from_file(full_filename):
     return np.fromfile(open(full_filename), dtype=np.complex64)
 
@@ -33,19 +36,54 @@ def get_filename_list():
             filelist.append(filename)
     return filelist
 
+# ==========================================================
+def get_packets(ary, pkt_size=500, mov_wdw_s=100):
+    abs_ary = np.abs(ary)
+
+    mov_wdw = np.ones((mov_wdw_s, ))
+    mov_avg = np.zeros(ary.shape)
+
+    mov_avg = np.convolve(abs_ary, mov_wdw/mov_wdw_s)
+
+    mov_avg_threshold = (np.max(mov_avg) + np.mean(mov_avg))/2
+    above_threshold = np.where(mov_avg > mov_avg_threshold)[0]
+
+    if len(above_threshold) == 0:
+        print("Threhold is invalid")
+        return None
+
+    pkt_ret = None
+    print(f"{above_threshold = }")
+
+
+    pass
+
+# ==========================================================
 def main():
 
     filename_list = get_filename_list()
     for filename in filename_list:
+        print("-"*25)
         full_filename = f"{args.s}{filename}"
         if not os.path.exists(full_filename):
             print(f"{full_filename} is not a file.")
             continue
 
-        print(f"Processing {full_filename}")
+        print(f"Processing {full_filename}...")
         data = load_dat_from_file(full_filename)
-    
 
+        if INSPECT:
+            data = data[0:100_000]
+
+        packets = get_packets(data)
+        if packets is None:
+            print(f"Processe {full_filename} failed. Not packet detected.")
+            continue
+
+        n_pkt = packets.shape[0]
+        print(f"Number of packet: {n_pkt}")
+
+        break
     pass
 
 if __name__ == '__main__':
