d7e970c7d02d2f35ce6bdf5a113245fbdbab86cd
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-24 19:30:16 -0400

    update

--- records/get_iot_packet.py
+++ records/get_iot_packet.py
@@ -5,12 +5,14 @@ import _pickle as pickle
 import argparse
 parser = argparse.ArgumentParser(description='save torch experience file to npy.')
 parser.add_argument('-s', help='source folder.', required=True)
-parser.add_argument('-n', help='source name.', required=True)
+parser.add_argument('-p', help='source pattern.', required=True)
 args = parser.parse_args()
+
+
+MOD_LIST = ["WIFI-BPSK", "WIFI-QPSK", "WIFI-16QAM", "WIFI-64QAM", "ZIGBEE-OQPSK", "BT-GFSK-LE1M", "BT-GFSK-LE2M", "BT-GFSK-S2Coding", "BT-GFSK-S2Coding"]
 
 if os.system("clear") != 0:
     os.system("cls")
-
 
 def load_dat_from_file(full_filename):
     return np.fromfile(open(full_filename), dtype=np.complex64)
@@ -20,15 +22,28 @@ def load_pickle_from_file():
         data = pickle.load(f, encoding='latin1')
     return data
 
+def files_under_folder(folder):
+    return os.listdir(folder)
+
+def get_filename_list():
+    filenames = files_under_folder(args.s)
+    filelist = []
+    for filename in filenames:
+        if filename.find(args.p) >= 0:
+            filelist.append(filename)
+    return filelist
+
 def main():
 
+    filename_list = get_filename_list()
+    for filename in filename_list:
+        full_filename = f"{args.s}{filename}"
+        if not os.path.exists(full_filename):
+            print(f"{full_filename} is not a file.")
+            continue
 
-    full_filename = f"{args.s}{args.n}"
-    if not os.path.exists(full_filename):
-        print(f"{full_filename} is not a file.")
-        exit()
-    
-    data = load_dat_from_file(full_filename)
+        print(f"Processing {full_filename}")
+        data = load_dat_from_file(full_filename)
     
 
     pass
