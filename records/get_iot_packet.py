b28d619aaada6f5d2efb965336f4b709ee3b9c77
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-24 20:17:16 -0400

    update

--- records/get_iot_packet.py
+++ records/get_iot_packet.py
@@ -50,6 +50,7 @@ def get_packets(ary, pkt_size=500, mov_wdw_s=250):
     max_v = np.max(mov_avg)
 
     mov_avg_threshold = (np.max(mov_avg) + np.mean(mov_avg)*2)/3
+    mov_avg_threshold = np.mean(mov_avg)
     above_threshold = np.where(mov_avg > mov_avg_threshold)[0]
     above_list = np.array([1 if x > mov_avg_threshold else 0 for x in mov_avg])
 
