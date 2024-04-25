c07f898510c604cbc2ab41cce035aa04f689a485
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-24 21:33:31 -0400

    update

--- records/get_iot_packet.py
+++ records/get_iot_packet.py
@@ -144,8 +144,7 @@ def main():
             data = data[20_000_000:30_000_000]
 
         n_batch = math.ceil(data.shape[0]/BATCH_SIZE)
-        print(f"Total batches: {n_batch}")
-        print(data.shape)
+        print(f"Total batches: {n_batch}, Total sample: {data.shape}")
 
         for n_b in range(n_batch):
             print(f"[{n_b+1}/{n_batch}]")
@@ -160,10 +159,10 @@ def main():
                 continue
 
             n_pkt = packets.shape[0]
-            print(f"{packets.shape = }")
             print(f"Number of packet: {n_pkt}")
-            save_batch_packets_name = f"B{n_b+1}-{n_batch}_{filename_prefix}.pkl"
+            save_batch_packets_name = f"{args.s}B{n_b+1}-{n_batch}_{filename_prefix}.pkl"
             print(f"{save_batch_packets_name = }")
+
         break
     pass
 
