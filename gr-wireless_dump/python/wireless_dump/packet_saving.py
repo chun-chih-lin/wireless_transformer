bbf67b1ebd328d3bb2d98c9703e7a0dac41a0891
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-04-26 15:13:44 -0400

    update

--- gr-wireless_dump/python/wireless_dump/packet_saving.py
+++ gr-wireless_dump/python/wireless_dump/packet_saving.py
@@ -39,6 +39,9 @@ class packet_saving(gr.sync_block):
         self.init = True
         self.update_save_filename()
 
+    def d_msg(self, msg):
+        if self.debug:
+            print(msg)
 
     # ----------------------------------------------
     # Callback functions
@@ -85,15 +88,17 @@ class packet_saving(gr.sync_block):
     # ----------------------------------------------
     def work(self, input_items, output_items):
         try:
-
             in0 = input_items[0]
             in1 = input_items[1]
 
             moving_avg_ret = in1.real
             # thresold = in1.imag
-
             above_threshold = np.where(moving_avg_ret > self.threshold)
 
+            i = 0
+            # while len(in0) > 0 and len(in1) > 0:
+            #     pass
+            self.d_msg(f"{len(in0) = }, {len(in1) = }")
 
             self.consume(0, len(in0))
             self.consume(1, len(in1))
