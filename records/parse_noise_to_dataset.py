3d680f0ae54c7e1582e610388b932ab245b5c8f1
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-05-01 15:17:07 -0400

    update

--- records/parse_noise_to_dataset.py
+++ records/parse_noise_to_dataset.py
@@ -12,13 +12,32 @@ TP = -10
 DIS = [5, 10, 15]
 SR = [5, 20]
 INTER = [0, 1]
+CF = 2360
+
+def get_filename(prefix, comb):
+    return f"Non_process_{prefix}_RAND_TP{TP}_D{comb[0]}_SR{comb[1]}_CF{CF}_I{comb[2]}.dat"
+
 
 def get_noise_files_from_source():
+
+    prefix = args.s.split("/")[1]
+    print(f"{prefix = }")
 
     combinations = itertools.product(DIS, SR, INTER)
 
     for comb in combinations:
         print(list(comb))
+        filename = get_filename(prefix, comb)
+        full_filename = f"{args.s}{filename}"
+
+
+        print("-"*50)
+        print(f"Checking {full_filename}...")
+        if os.path.exists(full_filename):
+            print("Success")
+            pass
+        else:
+            print(f"{full_filename} is not exist.")
 
     pass
 
