b531afed6216925bce0ac994e3418701f397cbf2
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-05-01 15:44:02 -0400

    update

---
+++ records/New_dataset_May/np_to_pickle.py
@@ -0,0 +1,18 @@
+import numpy as np
+import sys, os
+import itertools
+import _pickle as pickle
+
+import argparse
+
+parser = argparse.ArgumentParser(description='save torch experience file to npy.')
+parser.add_argument('-s', help='source torch experience file directory.', required=True)
+parser.add_argument('-y', help='Yes to all', default=False, action='store_true')
+args = parser.parse_args()
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
