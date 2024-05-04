e2b99c704f8c116955174a63e9d4bb7a5b8c796b
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-05-04 16:44:14 -0400

    update

--- records/Matlab/generate_continue_protocol_signal.m
+++ records/Matlab/generate_continue_protocol_signal.m
@@ -12,30 +12,30 @@ bt_LE500k_waveform = bleWaveformGenerator(message, Mode="LE500K");
 
 bt_LE125k_waveform = bleWaveformGenerator(message, Mode="LE125K");
 
-figure()
-subplot(2, 2, 1)
-plot(real(bt_LE1M_waveform))
-hold on
-plot(imag(bt_LE1M_waveform))
-xlim([0, 200])
-
-subplot(2, 2, 2)
-plot(real(bt_LE2M_waveform))
-hold on
-plot(imag(bt_LE2M_waveform))
-xlim([0, 200])
-
-subplot(2, 2, 3)
-plot(real(bt_LE500k_waveform))
-hold on
-plot(imag(bt_LE500k_waveform))
-xlim([0, 200])
-
-subplot(2, 2, 4)
-plot(real(bt_LE125k_waveform))
-hold on
-plot(imag(bt_LE125k_waveform))
-xlim([0, 200])
+% figure()
+% subplot(2, 2, 1)
+% plot(real(bt_LE1M_waveform))
+% hold on
+% plot(imag(bt_LE1M_waveform))
+% xlim([0, 200])
+% 
+% subplot(2, 2, 2)
+% plot(real(bt_LE2M_waveform))
+% hold on
+% plot(imag(bt_LE2M_waveform))
+% xlim([0, 200])
+% 
+% subplot(2, 2, 3)
+% plot(real(bt_LE500k_waveform))
+% hold on
+% plot(imag(bt_LE500k_waveform))
+% xlim([0, 200])
+% 
+% subplot(2, 2, 4)
+% plot(real(bt_LE125k_waveform))
+% hold on
+% plot(imag(bt_LE125k_waveform))
+% xlim([0, 200])
 
 
 zigbee_psdu_len = 127;
@@ -44,11 +44,11 @@ spc = 8;
 zigbee_cfg = lrwpanOQPSKConfig("Band", 2450, "SamplesPerChip", spc, "PSDULength", zigbee_psdu_len);
 waveOQPSK = lrwpanWaveformGenerator(psdu, zigbee_cfg);
 
-figure();
-plot(real(waveOQPSK))
-hold on
-plot(imag(waveOQPSK))
-xlim([0, 200])
+% figure();
+% plot(real(waveOQPSK))
+% hold on
+% plot(imag(waveOQPSK))
+% xlim([0, 200])
 
 
 nonHT_BPSK_cfg = wlanNonHTConfig(...
@@ -76,24 +76,119 @@ QAM64_psdu = randi([0 1], 8*nonHT_64QAM_cfg.PSDULength, 1, 'int8');
 [range, numBits] = scramblerRange(nonHT_BPSK_cfg);
 BPSK_scramInit = randi(range);
 nonHT_BPSK_waveform = wlanNonHTData(BPSK_psdu, nonHT_BPSK_cfg, BPSK_scramInit);
+disp("BPSK Length:")
+size(nonHT_BPSK_waveform, 1)
 
+disp("QPSK")
 [range, numBits] = scramblerRange(nonHT_QPSK_cfg);
 QPSK_scramInit = randi(range);
 nonHT_QPSK_waveform = wlanNonHTData(QPSK_psdu, nonHT_QPSK_cfg, QPSK_scramInit);
+disp("QPSK Length:")
+size(nonHT_QPSK_waveform, 1)
+while length(nonHT_QPSK_waveform) < length(nonHT_BPSK_waveform)
+    [range, numBits] = scramblerRange(nonHT_QPSK_cfg);
+    QPSK_scramInit = randi(range);
+    temp = wlanNonHTData(QPSK_psdu, nonHT_QPSK_cfg, QPSK_scramInit);
+    nonHT_QPSK_waveform = cat(1, nonHT_QPSK_waveform, temp);
+    disp("QPSK Length: ")
+    size(nonHT_QPSK_waveform, 1)
+end
 
+
+disp("16QAM")
 [range, numBits] = scramblerRange(nonHT_16QAM_cfg);
 QAM16_scramInit = randi(range);
 nonHT_16QAM_waveform = wlanNonHTData(QAM16_psdu, nonHT_16QAM_cfg, QAM16_scramInit);
+disp("16QAM Length:")
+size(nonHT_16QAM_waveform, 1)
+while length(nonHT_16QAM_waveform) < length(nonHT_BPSK_waveform)
+    [range, numBits] = scramblerRange(nonHT_16QAM_cfg);
+    QAM16_scramInit = randi(range);
+    temp = wlanNonHTData(QPSK_psdu, nonHT_16QAM_cfg, QAM16_scramInit);
+    nonHT_16QAM_waveform = cat(1, nonHT_16QAM_waveform, temp);
+    disp("16QAM Length: ")
+    size(nonHT_16QAM_waveform, 1)
+end
 
+disp("64QAM")
 [range, numBits] = scramblerRange(nonHT_64QAM_cfg);
 QAM64_scramInit = randi(range);
 nonHT_64QAM_waveform = wlanNonHTData(QAM64_psdu, nonHT_64QAM_cfg, QAM64_scramInit);
+disp("64QAM Length:")
+size(nonHT_16QAM_waveform, 1)
+while length(nonHT_64QAM_waveform) < length(nonHT_BPSK_waveform)
+    [range, numBits] = scramblerRange(nonHT_64QAM_cfg);
+    QAM64_scramInit = randi(range);
+    temp = wlanNonHTData(QPSK_psdu, nonHT_64QAM_cfg, QAM64_scramInit);
+    nonHT_64QAM_waveform = cat(1, nonHT_64QAM_waveform, temp);
+    disp("64QAM Length: ")
+    size(nonHT_64QAM_waveform, 1)
+end
+disp("----------------------------")
+
+size(nonHT_BPSK_waveform, 1)
+size(nonHT_QPSK_waveform, 1)
+size(nonHT_16QAM_waveform, 1)
+size(nonHT_64QAM_waveform, 1)
 
 
-size(nonHT_BPSK_waveform)
-size(nonHT_QPSK_waveform)
-size(nonHT_16QAM_waveform)
-size(nonHT_64QAM_waveform)
+save("BPSK_data_only.mat", 'nonHT_BPSK_waveform')
+save("QPSK_data_only.mat", 'nonHT_QPSK_waveform')
+save("16QAM_data_only.mat", 'nonHT_16QAM_waveform')
+save("64QAM_data_only.mat", 'nonHT_64QAM_waveform')
+
+
+
+
+n_sym = 100;
+s = 100*80 + 1;
+e = s + 63;
+
+BPSK_fft_ret = fftshift(fft(nonHT_BPSK_waveform(s:e)));
+QPSK_fft_ret = fftshift(fft(nonHT_QPSK_waveform(s:e)));
+QAM16_fft_ret = fftshift(fft(nonHT_16QAM_waveform(s:e)));
+QAM64_fft_ret = fftshift(fft(nonHT_64QAM_waveform(s:e)));
+
+figure()
+subplot(2, 2, 1)
+plot(real(BPSK_fft_ret))
+hold on
+plot(imag(BPSK_fft_ret))
+xlim([1, 64])
+
+subplot(2, 2, 2)
+plot(real(QPSK_fft_ret))
+hold on
+plot(imag(QPSK_fft_ret))
+xlim([1, 64])
+
+subplot(2, 2, 3)
+plot(real(QAM16_fft_ret))
+hold on
+plot(imag(QAM16_fft_ret))
+xlim([1, 64])
+
+subplot(2, 2, 4)
+plot(real(QAM64_fft_ret))
+hold on
+plot(imag(QAM64_fft_ret))
+xlim([1, 64])
+
+
+figure()
+subplot(2, 2, 1)
+plot(real(BPSK_fft_ret), imag(BPSK_fft_ret), '.')
+
+subplot(2, 2, 2)
+plot(real(QPSK_fft_ret), imag(QPSK_fft_ret), '.')
+
+subplot(2, 2, 3)
+plot(real(QAM16_fft_ret), imag(QAM16_fft_ret), '.')
+
+subplot(2, 2, 4)
+plot(real(QAM64_fft_ret), imag(QAM64_fft_ret), '.')
+
+
 
 size(waveOQPSK)
 
