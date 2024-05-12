16bcc487a52f044981b6b9ab6e213e028f44d116
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-05-11 21:00:04 -0400

    update

--- records/parse_semicolon_to_underscore.py
+++ records/parse_semicolon_to_underscore.py
@@ -1,9 +1,19 @@
 import sys, os
+import argparse
 
+if os.system("clear") != 0:
+    os.system("cls")
 
+parser = argparse.ArgumentParser(description='save torch experience file to npy.')
+parser.add_argument('-s', '--source_folder', help='source torch experience file directory.', required=True)
+parser.add_argument('-y', help='yes to all', action='store_true')
+args = parser.parse_args()
 
 
 def main():
+    folders = os.listdir(args.source_folder)
+    print(f"{folders = }")
+
     pass
 
 
