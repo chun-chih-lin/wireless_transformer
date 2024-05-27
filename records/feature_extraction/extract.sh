7d82917baf4b1da4898e3fdc1088780c0471fc61
Chun-Chih Lin <chunchi@g.clemson.edu>
2024-05-26 21:58:29 -0400

    update

--- records/feature_extraction/extract.sh
+++ records/feature_extraction/extract.sh
@@ -1,34 +1,50 @@
 #!/bin/bash
 
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p H -i 0 -d 5  -t 5 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p H -i 0 -d 10 -t 5 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p H -i 0 -d 15 -t 5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p H -i 1 -d 5  -t 5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p H -i 1 -d 10 -t 5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p H -i 1 -d 15 -t 5 -y
 
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p H -i 0 -d 5  -t 0 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p H -i 0 -d 10 -t 0 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p H -i 0 -d 15 -t 0 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p H -i 1 -d 5  -t 0 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p H -i 1 -d 10 -t 0 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p H -i 1 -d 15 -t 0 -y
 
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p H -i 0 -d 5  -t -5 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p H -i 0 -d 10 -t -5 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p H -i 0 -d 15 -t -5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p H -i 1 -d 5  -t -5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p H -i 1 -d 10 -t -5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p H -i 1 -d 15 -t -5 -y
 
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p H -i 0 -d 5  -t -10 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p H -i 0 -d 10 -t -10 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p H -i 0 -d 15 -t -10 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p H -i 1 -d 5  -t -10 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p H -i 1 -d 10 -t -10 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p H -i 1 -d 15 -t -10 -y
 
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p R -i 0 -d 5  -t 5 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p R -i 0 -d 10 -t 5 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p R -i 0 -d 15 -t 5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p O -i 1 -d 5  -t 5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p O -i 1 -d 10 -t 5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p O -i 1 -d 15 -t 5 -y
 
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p R -i 0 -d 5  -t 0 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p R -i 0 -d 10 -t 0 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p R -i 0 -d 15 -t 0 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p O -i 1 -d 5  -t 0 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p O -i 1 -d 10 -t 0 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p O -i 1 -d 15 -t 0 -y
 
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p R -i 0 -d 5  -t -5 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p R -i 0 -d 10 -t -5 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p R -i 0 -d 15 -t -5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p O -i 1 -d 5  -t -5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p O -i 1 -d 10 -t -5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p O -i 1 -d 15 -t -5 -y
 
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p R -i 0 -d 5  -t -10 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p R -i 0 -d 10 -t -10 -y
-python.exe ./extraction_0521.py -s ../Dataset_21mod_0521/ -p R -i 0 -d 15 -t -10 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p O -i 1 -d 5  -t -10 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p O -i 1 -d 10 -t -10 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p O -i 1 -d 15 -t -10 -y
+
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p R -i 1 -d 5  -t 5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p R -i 1 -d 10 -t 5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p R -i 1 -d 15 -t 5 -y
+
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p R -i 1 -d 5  -t 0 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p R -i 1 -d 10 -t 0 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p R -i 1 -d 15 -t 0 -y
+
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p R -i 1 -d 5  -t -5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p R -i 1 -d 10 -t -5 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p R -i 1 -d 15 -t -5 -y
+
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p R -i 1 -d 5  -t -10 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p R -i 1 -d 10 -t -10 -y
+python.exe ./extraction_10mod.py -s ../Dataset_10mod_0524/ -p R -i 1 -d 15 -t -10 -y
 
